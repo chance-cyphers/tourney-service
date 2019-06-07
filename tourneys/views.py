e89f070c522834f672bc8743192f730888cc106a
bob francis <bob@bobs-MacBook-Pro.local>
2019-06-07 12:15:39 -0400

    Fix cors i hope

--- tourneys/views.py
+++ tourneys/views.py
@@ -1,9 +1,11 @@
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
 from tourneys.models import Bracket
 from tourneys.serializers import BracketSerializer
 
 
+@csrf_exempt
 def index(request):
     bracket = Bracket(32, "basketball or something")
     serializer = BracketSerializer(bracket)
